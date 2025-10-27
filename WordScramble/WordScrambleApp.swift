2df797a5553c5a9e91cf8485b8f5794311bb63d2
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-26 18:20:04 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Максим Минаков on 26.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
