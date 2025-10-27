cd0eb8d189f64219d67d0f466ee2e603374eb3f4
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-10-26 19:54:47 -0700

    day 29

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -9,6 +9,16 @@ struct ContentView: View {
             Text($0)
         }
     }
+    
+    func testStrings(){
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+    }
 }
 
 #Preview {
